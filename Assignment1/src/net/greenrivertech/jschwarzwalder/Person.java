6de8454acc7c062a547ec380c6ba85cee108436f
Jami <jamischwarzwalder@gmail.com>
2016-06-28 22:48:00 -0700

    Assignment 1

---
+++ Assignment1/src/net/greenrivertech/jschwarzwalder/Person.java
@@ -0,0 +1,32 @@
+package net.greenrivertech.jschwarzwalder;
+
+/**
+ * Created by Jami on 7/7/2016.
+ */
+public class Person {
+
+
+
+    private int number;
+
+    private String name;
+
+    public int getNumber (){
+
+        return number;
+
+    }
+
+    public void setNumber(int n) {
+
+        number = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
