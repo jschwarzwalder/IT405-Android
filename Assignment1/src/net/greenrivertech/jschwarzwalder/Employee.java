7dac3a8fc64d77ac3157946bc583c6e1a54b0d08
Jami <jamischwarzwalder@gmail.com>
2016-06-30 05:54:56 -0700

    Adding required fields to Employee

---
+++ Assignment1/src/net/greenrivertech/jschwarzwalder/Employee.java
@@ -0,0 +1,16 @@
+package net.greenrivertech.jschwarzwalder;
+
+import java.util.Date;
+
+/**
+ * Created by Jami on 6/30/2016.
+ */
+public class Employee {
+    public Employee (){
+        int empID;
+        String name;
+        Character sex;
+        Date birthday;
+        Date joining_date;
+    }
+}
