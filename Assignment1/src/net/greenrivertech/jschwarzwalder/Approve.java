6e178575ed61512a495f222ae96e5ae28d3b19cc
Jami <jamischwarzwalder@gmail.com>
2016-07-01 00:26:37 -0700

    Built Interface

---
+++ Assignment1/src/net/greenrivertech/jschwarzwalder/Approve.java
@@ -0,0 +1,11 @@
+package net.greenrivertech.jschwarzwalder;
+
+/**
+ * Created by Jami on 6/30/2016.
+ */
+public interface Approve {
+
+    public void approvePurchase();
+
+    public void approvePurchase(int value);
+}
---
+++ Assignment1/src/net/greenrivertech/jschwarzwalder/Sign.java
@@ -0,0 +1,12 @@
+package net.greenrivertech.jschwarzwalder;
+
+/**
+ * Created by Jami on 6/30/2016.
+ */
+public interface Sign {
+
+    public void signCheck();
+
+    public void signCheck(int value);
+
+}
