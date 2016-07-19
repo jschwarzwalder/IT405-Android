458263727d19f0e0423de8f373ad65c11fa0e938
Jami <jamischwarzwalder@gmail.com>
2016-07-19 00:09:51 -0700

    Android Week 2 Stock Layout XML

---
+++ IT405/StockUI/app/src/main/java/net/greenrivertech/jschwarzwalder/stockui/MainScreen.java
@@ -0,0 +1,14 @@
+package net.greenrivertech.jschwarzwalder.stockui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(net.greenrivertech.jschwarzwalder.stocklayouts.R.layout.activity_main_screen);
+
+    }
+}
