a5b21fd84ddfb75f0433e51d7d150a5fca54ce7a
Jami <jamischwarzwalder@gmail.com>
2016-07-25 20:55:31 -0700

    onClick listeners delete working

--- IT405/StockPro/app/src/main/java/net/greenrivertech/jschwarzwalder/stockpro/MainActivity.java
+++ IT405/StockPro/app/src/main/java/net/greenrivertech/jschwarzwalder/stockpro/MainActivity.java
@@ -20,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
     }
+
+    protected void onResume(Bundle savedInstanceState) {
+
+
+    }
+
     public void onAmazonDelete(View v) {
         Log.d(TAG, "clicked Amazon Delete");
 
@@ -30,68 +36,48 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.amznValue).setVisibility(View.GONE);
         findViewById(R.id.amznDelete).setVisibility(View.GONE);
     }
-    protected void onResume(Bundle savedInstanceState){
-        final Button deleteAmazonButton = (Button) findViewById(R.id.amznDelete);
-        deleteAmazonButton.setOnClickListener(new View.OnClickListener() {
 
-        });
+    public void onNintendoDelete(View v) {
+        Log.d(TAG, "clicked Nintendo Delete");
 
-        final Button deleteMicrosoftButton = (Button) findViewById(R.id.msftDelete);
-        deleteMicrosoftButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d(TAG, "clicked Microsoft Delete");
+        findViewById(R.id.nintPrice).setVisibility(View.GONE);
+        findViewById(R.id.nintQuantity).setVisibility(View.GONE);
+        findViewById(R.id.nintChange).setVisibility(View.GONE);
+        findViewById(R.id.nintName).setVisibility(View.GONE);
+        findViewById(R.id.nintValue).setVisibility(View.GONE);
+        findViewById(R.id.nintDelete).setVisibility(View.GONE);
+    }
 
-                findViewById(R.id.msftPrice).setVisibility(View.GONE);
-                findViewById(R.id.msftQuantity).setVisibility(View.GONE);
-                findViewById(R.id.msftChange).setVisibility(View.GONE);
-                findViewById(R.id.msftName).setVisibility(View.GONE);
-                findViewById(R.id.msftValue).setVisibility(View.GONE);
-                findViewById(R.id.msftDelete).setVisibility(View.GONE);
-            }
-        });
+    public void onSpringLeafDelete(View v) {
+        Log.d(TAG, "clicked Leaf Delete");
 
-        final Button deleteNetflixButton = (Button) findViewById(R.id.netfDelete);
-        deleteNetflixButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d(TAG, "clicked Netflix Delete");
+        findViewById(R.id.leafPrice).setVisibility(View.GONE);
+        findViewById(R.id.leafQuantity).setVisibility(View.GONE);
+        findViewById(R.id.leafChange).setVisibility(View.GONE);
+        findViewById(R.id.leafName).setVisibility(View.GONE);
+        findViewById(R.id.leafValue).setVisibility(View.GONE);
+        findViewById(R.id.leafDelete).setVisibility(View.GONE);
+    }
 
-                findViewById(R.id.netfPrice).setVisibility(View.GONE);
-                findViewById(R.id.netfQuantity).setVisibility(View.GONE);
-                findViewById(R.id.netfChange).setVisibility(View.GONE);
-                findViewById(R.id.netfName).setVisibility(View.GONE);
-                findViewById(R.id.netfValue).setVisibility(View.GONE);
-                findViewById(R.id.netfDelete).setVisibility(View.GONE);
-            }
-        });
+    public void onNetflixDelete(View v) {
+        Log.d(TAG, "clicked Netflix Delete");
 
-        final Button deleteSpringLeafButton = (Button) findViewById(R.id.leafDelete);
-        deleteSpringLeafButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d(TAG, "clicked Leaf Delete");
+        findViewById(R.id.netfPrice).setVisibility(View.GONE);
+        findViewById(R.id.netfQuantity).setVisibility(View.GONE);
+        findViewById(R.id.netfChange).setVisibility(View.GONE);
+        findViewById(R.id.netfName).setVisibility(View.GONE);
+        findViewById(R.id.netfValue).setVisibility(View.GONE);
+        findViewById(R.id.netfDelete).setVisibility(View.GONE);
+    }
 
-                findViewById(R.id.leafPrice).setVisibility(View.GONE);
-                findViewById(R.id.leafQuantity).setVisibility(View.GONE);
-                findViewById(R.id.leafChange).setVisibility(View.GONE);
-                findViewById(R.id.leafName).setVisibility(View.GONE);
-                findViewById(R.id.leafValue).setVisibility(View.GONE);
-                findViewById(R.id.leafDelete).setVisibility(View.GONE);
-            }
-        });
+    public void onMicrosoftDelete(View v) {
+        Log.d(TAG, "clicked Microsoft Delete");
 
-        final Button deleteNintendoButton = (Button) findViewById(R.id.nintDelete);
-        deleteNintendoButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d(TAG, "clicked Nintendo Delete");
-
-                findViewById(R.id.nintPrice).setVisibility(View.GONE);
-                findViewById(R.id.nintQuantity).setVisibility(View.GONE);
-                findViewById(R.id.nintChange).setVisibility(View.GONE);
-                findViewById(R.id.nintName).setVisibility(View.GONE);
-                findViewById(R.id.nintValue).setVisibility(View.GONE);
-                findViewById(R.id.nintDelete).setVisibility(View.GONE);
-            }
-        });
-
-
+        findViewById(R.id.msftPrice).setVisibility(View.GONE);
+        findViewById(R.id.msftQuantity).setVisibility(View.GONE);
+        findViewById(R.id.msftChange).setVisibility(View.GONE);
+        findViewById(R.id.msftName).setVisibility(View.GONE);
+        findViewById(R.id.msftValue).setVisibility(View.GONE);
+        findViewById(R.id.msftDelete).setVisibility(View.GONE);
     }
 }
