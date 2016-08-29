61bba2bfd4402f6cadc527df4fa6daa2cab185e8
Jami <jamischwarzwalder@gmail.com>
2016-08-29 16:56:55 -0700

    Building Layout Files, and Spash Screen

---
+++ IT405/StockProFinal/app/src/main/java/net/greenrivertech/jschwarzwalder/stockprofinal/StockQuote.java
@@ -0,0 +1,13 @@
+package net.greenrivertech.jschwarzwalder.stockprofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StockQuote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_quote);
+    }
+}
