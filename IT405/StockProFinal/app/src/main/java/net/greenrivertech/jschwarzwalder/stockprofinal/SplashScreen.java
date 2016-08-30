84eda5e0128202c8888b8216f9cb84a0a0ece0d0
Jami <jamischwarzwalder@gmail.com>
2016-08-29 18:10:05 -0700

    Added Intent to splash screen

--- IT405/StockProFinal/app/src/main/java/net/greenrivertech/jschwarzwalder/stockprofinal/SplashScreen.java
+++ IT405/StockProFinal/app/src/main/java/net/greenrivertech/jschwarzwalder/stockprofinal/SplashScreen.java
@@ -1,7 +1,10 @@
 package net.greenrivertech.jschwarzwalder.stockprofinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        View.OnClickListener screen = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //advance to next screen
+				showMain();
+            }
+        };
+        View layout = findViewById(R.id.splash);
+        layout.setOnClickListener(screen);
     }
-}
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        //wait 10 seconds
+
+        //advance to next screen
+		showMain();
+    }
+
+      public void showMain() {
+        Intent i = new Intent(this, MainScreen.class);
+        startActivity(i);
+    }
+}     
